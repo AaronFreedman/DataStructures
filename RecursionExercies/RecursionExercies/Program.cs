6d7360f435420a527e6e60a68dffc376fcd0a38e
unknown <aaronlfreedman@aol.com>
2018-12-09 14:31:57 -0800

    Recursive Functions

--- RecursionExercies/RecursionExercies/Program.cs
+++ RecursionExercies/RecursionExercies/Program.cs
@@ -4,9 +4,37 @@ namespace RecursionExercies
 {
     class Program
     {
+
+        static int Factorial(int start)
+        {
+            //base/escape case
+            if(start <= 1)
+            {
+                return 1;
+            }
+            
+            return start * Factorial(start - 1);
+        }
+
+        static int Fibb(int index)
+        {
+            //base/escape case
+            if (index <= 1)
+            {
+                return 0;
+            }
+            if (index == 2)
+            {
+                return 1;
+            }
+
+            return Fibb(index - 1) + Fibb(index - 2);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.ReadKey();
         }
     }
 }
